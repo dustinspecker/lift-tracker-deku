0edd583f757e7bd6d83951bf229d1a500d82a206
Dustin Specker <dustinspecker@dustinspecker.com>
2016-01-17 13:15:37 -0600

    feat(WeightList): connect handles own props and children
    Pass children through to component. Merge own props with mapped state
    and actions.

--- src/components/weight-list.jsx
+++ src/components/weight-list.jsx
@@ -5,25 +5,25 @@ import {removeWeight} from '../actions/weights'
 import Weight from './weight'
 
 const removeHandler = (remove, index) => () => remove(index)
-const WeightList = ({remove, weights}) => <div>
-  {weights.toJS().map((weight, index) =>
+const WeightList = ({props}) => <div>
+  {props.weights.toJS().map((weight, index) =>
     <Weight
       key={weight.id}
       name={weight.name}
-      remove={removeHandler(remove, index)}
+      remove={removeHandler(props.remove, index)}
     />
   )}
 </div>
 
 const mapStateToProps = ({weights}) => ({weights})
 
-const connect = (mapFn, actions) => component => ({context, dispatch}) => {
+const connect = (mapFn, actions) => component => ({children = {}, context = {}, dispatch = () => ({}), props = {}}) => {
   const mappedActions = Object.keys(actions).reduce((acc, action) => {
     acc[action] = (...args) => dispatch(actions[action](...args))
     return acc
   }, {})
-  const props = objectAssign(mapFn(context), mappedActions)
-  return component(props)
+  const newProps = objectAssign(props, mapFn(context), mappedActions)
+  return component({children, props: newProps})
 }
 
 export default connect(
