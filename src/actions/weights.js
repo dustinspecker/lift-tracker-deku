d8e28011bf2d6f05a2422146fc57d18cd65c84b0
Dustin Specker <dustinspecker@dustinspecker.com>
2016-01-17 12:10:23 -0600

    feat(actions): add removeWeight

--- src/actions/weights.js
+++ src/actions/weights.js
@@ -1,3 +1,8 @@
+export const removeWeight = index => ({
+  type: 'REMOVE_WEIGHT',
+  index
+})
+
 export const setWeights = weights => ({
   type: 'SET_WEIGHTS',
   weights
--- tests/actions/test-weights.js
+++ tests/actions/test-weights.js
@@ -3,6 +3,13 @@ import test from 'ava'
 
 import * as actions from '../../src/actions/weights'
 
+test('should return REMOVE_WEIGHT action', t => {
+  const {index, type} = actions.removeWeight(1)
+
+  t.is(type, 'REMOVE_WEIGHT')
+  t.is(index, 1)
+})
+
 test('should return SET_WEIGHTS action', t => {
   const {type, weights} = actions.setWeights(List.of(1, 2, 3))
 
