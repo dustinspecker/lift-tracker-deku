9437f0d4426e539a5a7a13094cdc2ea13fc23ff5
Dustin Specker <dustinspecker@dustinspecker.com>
2016-01-28 17:18:07 -0600

    chore(deps): update deku-prop-types

--- src/components/weight.jsx
+++ src/components/weight.jsx
@@ -1,10 +1,10 @@
 import {element} from 'deku'
-import {propTypes, validate} from 'deku-prop-types'
+import {PropTypes, validate} from 'deku-prop-types'
 
 const Weight = ({props}) => <div onClick={props.remove}>{props.name}</div>
 
 Weight.propTypes = {
-  name: propTypes.string.isRequired
+  name: PropTypes.string.isRequired
 }
 
 export default validate(Weight)
